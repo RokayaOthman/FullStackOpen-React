fdc5eb8dfc67f568195e5ffcf12004ea59eca972
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-07 00:43:50 +0200

    Create Statistics component

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,9 +1,12 @@
 import { useState } from 'react'
 
-// defining a statistics component
+function Statistics(props) {
+  return (
+       <div>{props.word} : {props.value}{props.additional}</div>
+  );
+}
 
-
-const App = () => {
+const App = () => { 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
@@ -20,13 +23,20 @@ const App = () => {
     <h2><strong>Statistics</strong></h2>
     <p>No feedback given</p>
       {/* <div>{good} {neutral} {bad}</div> */}
-    <div>good {good}</div>
+    {/* <div>good {good}</div>
     <div>neutral {neutral}</div>
     <div>bad {bad}</div>
     <div>all {all}</div>
     <div>average {all/3}</div>
-    <div>positive {pos}%</div>
+    <div>positive {pos}%</div> 
     
+    */}
+    <Statistics word="good" value={good}/>
+    <Statistics word="neutral" value={neutral}/>
+    <Statistics word="bad" value={bad}/>
+    <Statistics word="all" value={all}/>
+    <Statistics word="average" value={all / 3}/>
+    <Statistics word="positive" value={pos} additional="%"/>
     </>
       
     
