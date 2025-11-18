ff3f3815355bc92a7f00a54ab34b610fcd517551
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-18 22:08:18 +0200

    Update

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -45,8 +45,9 @@ const App = () => {
       ]
     }
   ] 
- // // // // // // // // // // //
- 
+ // // // // /
+ // / // // // // // //
+  // // // // 
   const totalE = course[0].parts.reduce((acc, part) => acc + (part.exercises) , 0 )
 
   const Header = (props) => {
