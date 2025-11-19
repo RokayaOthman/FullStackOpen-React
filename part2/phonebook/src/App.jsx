237afdcfc422092f05567bfb04024c0caafae9ba
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-11-19 22:55:21 +0200

    Solve Phonebook Part4 : search input

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -4,7 +4,7 @@ const App = () => {
   const [persons, setPersons] = useState([
     { name: 'Arto Hellas', number: '040-1234561', id: 1 },
     { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Dan Abramov', number: '12-43-2343455', id: 3 },
     { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
 
