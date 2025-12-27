6005fcc9c7e6786615b09ba40995724633ac9e15
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-12-28 00:07:21 +0200

    send data to the server using post

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -1,33 +1,24 @@
 import { useState } from 'react'
 import { useEffect } from 'react'
 import axios from 'axios'
+import numberService from './services/numbers'
+
 import PersonFormDisplay from './components/PersonForm'
 import PersonsDisplay from './components/persons'
 import FilterSearch from './components/Filter'
 
 const App = () => {
-  // The task is instead of using the data that is inside App.jsx we will fetch it from db.json
-  // const [persons, setPersons] = useState([
-  //   { name: 'Arto Hellas', number: '040-1234561', id: 1 },
-  //   { name: 'Ada Lovelace', number: '39-44-532523', id: 2 },
-  //   { name: 'Dan Abramov', number: '12-43-2343455', id: 3 },
-  //   { name: 'Mary Poppendieck', number: '39-23-64231222', id: 4 }
-  // ]) 
-// // // 
+
   const [persons, setPersons] = useState([])
-
-  useEffect(() => {
-    axios 
-    .get('http://localhost:3001/persons')
-    .then(Response => {
-      setPersons(Response.data)
-    })
-  }, [])
-
-  // to store user submitted input
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+
+  useEffect(() => {
+    numberService
+    .getAll()
+    .then(Response => setNewNumber(Response.data))
+  }, [])
 
   const addNameNumber = (event) => {
     event.preventDefault()
@@ -37,9 +28,19 @@ const App = () => {
         name: newName,
         number: newNumber,
        }
-      setPersons(persons.concat(personObject))
-      setNewName('')
-      setNewNumber('')
+       // TODO : change this 
+      // setPersons(persons.concat(personObject))
+      // setNewName('')
+      // setNewNumber('')
+      // 
+      numberService
+      .create(personObject)
+      .then(response => {
+        setPersons(persons.concat(personObject))
+        setNewName('')
+        setNewNumber('')
+      })
+
      }
     else {
       window.alert(`${newName} is already added!`)
