544c213e5ac2869434acb9d828ba32538d8a1104
rokayaothman2003@gmail.com <rokayaothman2003@gmail.com>
2025-12-29 22:06:12 +0200

    .

--- part2/phonebook/src/services/numbers.js
+++ part2/phonebook/src/services/numbers.js
@@ -11,7 +11,7 @@ const create = newObject => {
     const request = axios.post(baseUrl, newObject)
     return request.then((response) => response.data)
 }
-
+////
 const update = (id, newObject) => {
     const request = axios.put(`${baseUrl}/${id}`, newObject)
     return request.then((response) => response.data)
